8c44ab9a249321e173e7af8ca728409f9fecfbbe
Ben Savage <ben.savage2@icloud.com>
2025-01-03 09:46:44 -0500

    added new post route with error handler

--- server.js
+++ server.js
@@ -2,31 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(require("morgan")("dev"));
+app.use("/api", require("./api/index"));
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-const employees = require("./employees");
+app.use ((err,req,res,next) =>{
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message)
+})
 
-app.get("/employees", (req, res) => {
-  res.json(employees);
-});
-
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    res.status(404).send(`There is no employee with id ${id}.`);
-  }
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found." });
 });
 
 app.listen(PORT, () => {
-  `Listening on port ${PORT}...`;
+  console.log(`Listening on port ${PORT}...`);
 });
+
+
